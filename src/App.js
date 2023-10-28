1e62b4539a5f15c8650e8193fd7997badac94753
hamoudi8080 <hamoderaso@outlook.com>
2023-10-28 18:26:02 +0200

    header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
- 
+
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-  <span> das</span>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <span> das</span>
+      </div>
+    </>
   );
 }
 
