ea2576852394a15bed9c9d7c1259499c5ef538a0
hamoudi8080 <hamoderaso@outlook.com>
2023-11-17 18:11:22 +0100

    search page is done

--- src/pages/Series/Series.js
+++ src/pages/Series/Series.js
@@ -1,12 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
  
+import { useEffect } from "react";
+import { useState } from "react";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import Genres from "../../components/Genres/Genres";
+import useGenre from "../../hooks/useGenre";
+
 
 function Series() {
-    return (
-        <div>
-          <span className='pageTitle'>Trending</span>
-        </div>
-      )
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumberOfPages] = useState(1);
+  const [selectedGenres, setSelectedGenres ] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+    console.log(data);
+    setContent(data.results);
+    setNumberOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchMovies();
+    // eslint-disable-next-line
+  }, [genreforURL, page]);
+  
+  return (
+    <div>
+      <Genres
+        type="tv"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
+
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.release_date}
+              media_type="movie"
+              vote_average={c.vote_average.toFixed(1)}
+            />
+          ))}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numberOfPages={numOfPages} />
+      )}
+    </div>
+  );
 }
-export default Series
+export default Series;
